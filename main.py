abadc60147e868400684cf538fd299503a6172bb
A1ex-N <35363716+A1ex-N@users.noreply.github.com>
2021-06-23 14:57:24 +1000

    Added argument to call PrintURLs function

--- main.py
+++ main.py
@@ -8,9 +8,16 @@ def main():
     parser.add_argument("-f", "--folder", default="", help="Folder to download images to", required=False)
     parser.add_argument("-o", "--overwrite", default=False, action=argparse.BooleanOptionalAction, help="Overwrite existing files")
     parser.add_argument("-s", "--stopAfter", default=-1, type=int, help="Stop after X amount of files downloaded")
+    parser.add_argument("-p", "--print", default=False, action=argparse.BooleanOptionalAction, help="Print all image URLs without downloading")
+
     args = parser.parse_args()
 
     phdl = PHDL(args.url)
+    
+    if (args.print):
+        phdl.PrintURLs()
+        return
+
     phdl.DownloadImages(args.folder, overwrite=args.overwrite, stopAfter=args.stopAfter)
 
 if __name__ == "__main__":
