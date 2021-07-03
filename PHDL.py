cdcbce31416f07618bded3e679ac3a03f7206eb9
A1ex-N <35363716+A1ex-N@users.noreply.github.com>
2021-07-03 17:47:23 +1000

    Update PHDL.py

--- PHDL.py
+++ PHDL.py
@@ -2,103 +2,100 @@ import requests
 import json
 import os
 
+
 s = requests.Session()
 
 
-class _Internal:
-    def Download(url, iterator, amount, path):
-        print(f"Downloading {url} ({iterator}/{amount})")
-        img = s.get(url).content
-        with open(f"{path}", "wb+") as file:
-            file.write(img)
+def download(url, iterator, amount, path):
+    print(f"Downloading {url} ({iterator}/{amount})")
+    img = s.get(url).content
+    with open(f"{path}", "wb+") as file:
+        file.write(img)
 
-class PHDL():
-    jsonUrl = "https://www.pornhub.com/album/show_album_json?album="
-    albumId = ""
-    largeImgUrls = []
+
+class PHDL:
+    json_url = "https://www.pornhub.com/album/show_album_json?album="
+    album_id = ""
+    large_img_urls = []
 
     def __init__(self, url):
-        '''
-        Populates self.largeImgUrls and sets self.ablumId
+        """
+        Populates self.largeImgUrls and sets self.album_id
 
         Parameters:
             url (string): The URL to the pornhub album to download images from
-        '''
-        if (url == None):
-            return "You must set a URL to a pornhub album"
+        """
+        if url is None:
+            print("You must set a URL to a pornhub album")
+            exit()
 
-        self.albumId = url.split('/')[-1]
-        albumUrl = self.jsonUrl + self.albumId
+        self.album_id = url.split('/')[-1]
+        album_url = self.json_url + self.album_id
 
-        jsonResponse = s.get(albumUrl).text
-        jsonObject = json.loads(jsonResponse)
+        json_response = s.get(album_url).text
+        json_object = json.loads(json_response)
 
-        for object in jsonObject:
-            isFanOnly = jsonObject[object]["isFanOnly"]
-            largeImageUrl = jsonObject[object]["img_large"]
+        for child in json_object:
+            is_fan_only = json_object[child]["isFanOnly"]
+            large_image_url = json_object[child]["img_large"]
 
-            if isFanOnly == False:
-                self.largeImgUrls.append(largeImageUrl)
+            if is_fan_only is False:
+                self.large_img_urls.append(large_image_url)
 
             else:
-                print(f"{largeImageUrl} is fan only. Skipping.")
+                print(f"{large_image_url} is fan only. Skipping.")
 
-
-
-    def PrintURLs(self, showCounter = False):
-        '''
+    def print_urls(self, show_counter=False):
+        """
         Prints all image URL's
 
             Parameters:
-                showCounter (bool): Specifies whether or not to show a counter for each URL (e.g. (1/219)).
+                show_counter (bool): Specifies whether or not to show a counter for each URL (e.g. (1/219)).
                                     Default is False
-        '''
-        if len(self.largeImgUrls) == 0:
+        """
+        if len(self.large_img_urls) == 0:
             return "No images to download"
 
-        for i, url in enumerate(self.largeImgUrls, start=1):
-            if showCounter:
-                print(f"{url} ({i}/{len(self.largeImgUrls)})")
+        for i, url in enumerate(self.large_img_urls, start=1):
+            if show_counter:
+                print(f"{url} ({i}/{len(self.large_img_urls)})")
             else:
                 print(url)
 
-
-
-    def DownloadImages(self, folderName = "", overwrite = False, stopAfter = -1):
-        '''
+    def download_images(self, folder_name="", overwrite=False, stop_after=-1):
+        """
         Downloads all "img_large" images from a pornhub photo album
 
             Parameters:
-                folderName (string): The folder to download images to. Default is the album ID.
+                folder_name (string): The folder to download images to. Default is the album ID.
                 overwrite (bool): Specifies whether or not to overwrite existing files. Default is False.
-                stopAfter (int): Stop downloading images after x amount of images have been downloaded.
+                stop_after (int): Stop downloading images after x amount of images have been downloaded.
                                  default is -1 (all)
-        '''
-        amountOfImgs = len(self.largeImgUrls)
+        """
+        amount_of_imgs: int = len(self.large_img_urls)
 
-        if (amountOfImgs == 0):
+        if amount_of_imgs == 0:
             return "No images to download"
 
-        if (stopAfter == -1):
-            stopAfter = len(self.largeImgUrls)
+        if stop_after == -1:
+            stop_after = len(self.large_img_urls)
 
-        if folderName == "":
-            folderName = self.albumId
+        if folder_name == "":
+            folder_name = self.album_id
 
-        if (not os.path.isdir(folderName)):
-            os.mkdir(folderName)
+        if not os.path.isdir(folder_name):
+            os.mkdir(folder_name)
 
-
-        for i, url in enumerate(self.largeImgUrls, start=1):
+        for i, url in enumerate(self.large_img_urls, start=1):
             extension = url.split('.')[-1]
-            fullPath = f"{folderName}\{i}.{extension}"
-            if (i - 1 == stopAfter):
+            full_path = f"{folder_name}/{i}.{extension}"
+            if i - 1 == stop_after:
                 break
 
             if overwrite:
-                _Internal.Download(url, i, amountOfImgs, fullPath)
+                download(url, i, amount_of_imgs, full_path)
             else:
-                if not os.path.exists(fullPath):
-                    _Internal.Download(url, i, amountOfImgs, fullPath)
+                if not os.path.exists(full_path):
+                    download(url, i, amount_of_imgs, full_path)
                 else:
-                    print(f"{fullPath} already exists. Skipping. ({i}/{amountOfImgs})")
+                    print(f"{full_path} already exists. Skipping. ({i}/{amount_of_imgs})")
